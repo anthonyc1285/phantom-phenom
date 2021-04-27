b440c459382b4b527de9aaba11a00fa773d00650
Anthony Cirio <acirio@emoneyadvisor.com>
2021-04-27 10:09:16 -0400

    Checkboxe return Bulk File Item and directories

--- BulkFileConfigGenerator/Form1.cs
+++ BulkFileConfigGenerator/Form1.cs
@@ -25,22 +25,41 @@ namespace BulkFileConfigGenerator
 
         private void btnClick_Click(object sender, EventArgs e)
         {
-            var remote = remoteFileName.Text;
+            var remote = remoteFileName.Text;        
             var local = localFileName.Text;
             var configTemplate = checkedListBulkFiles.Text;
-           
+            int i;
+            var s = "";
+            for (i = 0; i <= (checkedListBulkFiles.Items.Count - 1); i++)
+            {
+                if (checkedListBulkFiles.GetItemChecked(i) && checkedListBulkFiles.Items[i].ToString() == "Relationship")
+                {
+                    s = @"<BulkFileItem>" + "\n" +
+                        @"<BcpTimeout>PT0S</BcpTimeout>" + "\n" +
+                        @"<ID>Reps</ID>" + "\n" + 
+                        @"<LocalFile>{13}\" + local + @"\{3:MMddyyyy}_Relationship_" + local + @".txt</LocalFile>" + "\n" + 
+                        @"<MaxRowsPerBatch i:nil=""true"" />" + "\n" + 
+                        @"<Optional>false</Optional>" + "\n" + 
+                        @"<RemoteFile>{0}\" + remote + @"\{3:MMddyyyy}_Relationship_" + remote + @".txt</RemoteFile>" + "\n" + 
+                        @"<UploadFileDescription i:nil=""true"" />" + "\n" +
+                        @"<UploadFileName i:nil=""true"" />" + "\n" + 
+                        @"</BulkFileItem>";
 
-            txtOutput.Text = configTemplate + " is working? " + remote + " is the RemoteFile, and " + local + " is the LocalFile.";
+                }
+            }
+            txtOutput.Text = s;
         }
+
+
 
         private void label_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void checkedListBulkFiles_SelectedIndexChanged(object sender, EventArgs e)
+        public void checkedListBulkFiles_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
