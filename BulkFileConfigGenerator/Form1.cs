f4167e5015688c864a576f848c8d469dee97633d
Anthony Cirio <acirio@emoneyadvisor.com>
2021-04-30 10:47:42 -0400

    getBulkFileIDs mehtod added

--- BulkFileConfigGenerator/Form1.cs
+++ BulkFileConfigGenerator/Form1.cs
@@ -25,29 +25,9 @@ namespace BulkFileConfigGenerator
 
         private void btnClick_Click(object sender, EventArgs e)
         {
-            var remote = remoteFileName.Text;        
-            var local = localFileName.Text;
             var configTemplate = checkedListBulkFiles.Text;
-            int i;
-            var s = "";
-            for (i = 0; i <= (checkedListBulkFiles.Items.Count - 1); i++)
-            {
-                if (checkedListBulkFiles.GetItemChecked(i) && checkedListBulkFiles.Items[i].ToString() == "Relationship")
-                {
-                    s = @"<BulkFileItem>" + "\n" +
-                        @"<BcpTimeout>PT0S</BcpTimeout>" + "\n" +
-                        @"<ID>Reps</ID>" + "\n" + 
-                        @"<LocalFile>{13}\" + local + @"\{3:MMddyyyy}_Relationship_" + local + @".txt</LocalFile>" + "\n" + 
-                        @"<MaxRowsPerBatch i:nil=""true"" />" + "\n" + 
-                        @"<Optional>false</Optional>" + "\n" + 
-                        @"<RemoteFile>{0}\" + remote + @"\{3:MMddyyyy}_Relationship_" + remote + @".txt</RemoteFile>" + "\n" + 
-                        @"<UploadFileDescription i:nil=""true"" />" + "\n" +
-                        @"<UploadFileName i:nil=""true"" />" + "\n" + 
-                        @"</BulkFileItem>";
-
-                }
-            }
-            txtOutput.Text = s;
+            getBulkFileIDs();
+           
         }
 
 
@@ -60,6 +40,82 @@ namespace BulkFileConfigGenerator
         public void checkedListBulkFiles_SelectedIndexChanged(object sender, EventArgs e)
         {
             
+        }
+
+        public void getBulkFileIDs()
+        {
+            while(checkedListBulkFiles.Items.Count > 0)
+            {
+                var remote = remoteFileName.Text;
+                var local = localFileName.Text;
+                var s = " ";
+                StringBuilder sb = new StringBuilder(s);
+
+                for (int i = 0; i <= (checkedListBulkFiles.Items.Count - 1); i++)
+                {
+                    if (checkedListBulkFiles.GetItemChecked(i) && checkedListBulkFiles.Items[i].ToString() == "Relationship")
+                    {
+                        var relationship = @"<BulkFileItem>" + "\n" +
+                            @"<BcpTimeout>PT0S</BcpTimeout>" + "\n" +
+                            @"<ID>Reps</ID>" + "\n" +
+                            @"<LocalFile>{13}\" + local + @"\{3:MMddyyyy}_Relationship_" + local + @".txt</LocalFile>" + "\n" +
+                            @"<MaxRowsPerBatch i:nil=""true"" />" + "\n" +
+                            @"<Optional>false</Optional>" + "\n" +
+                            @"<RemoteFile>{0}\" + remote + @"\{3:MMddyyyy}_Relationship_" + remote + @".txt</RemoteFile>" + "\n" +
+                            @"<UploadFileDescription i:nil=""true"" />" + "\n" +
+                            @"<UploadFileName i:nil=""true"" />" + "\n" +
+                            @"</BulkFileItem>" + "\n";
+                        sb.Insert(s.Length - 1, relationship);
+                    }
+                    else if (checkedListBulkFiles.GetItemChecked(i) && checkedListBulkFiles.Items[i].ToString() == "Advisor") //CHECK THE ID IN NEXUS
+                    {   
+                        var advisor = @"<BulkFileItem>" + "\n" +
+                           @"<BcpTimeout>PT0S</BcpTimeout>" + "\n" +
+                           @"<ID>Reps</ID>" + "\n" +
+                           @"<LocalFile>{13}\" + local + @"\{3:MMddyyyy}_Advisor_" + local + @".txt</LocalFile>" + "\n" +
+                           @"<MaxRowsPerBatch i:nil=""true"" />" + "\n" +
+                           @"<Optional>false</Optional>" + "\n" +
+                           @"<RemoteFile>{0}\" + remote + @"\{3:MMddyyyy}_Advisor_" + remote + @".txt</RemoteFile>" + "\n" +
+                           @"<UploadFileDescription i:nil=""true"" />" + "\n" +
+                           @"<UploadFileName i:nil=""true"" />" + "\n" +
+                           @"</BulkFileItem>" + "\n";
+                        sb.Insert(s.Length - 1, advisor);
+                    }
+                    else if (checkedListBulkFiles.GetItemChecked(i) && checkedListBulkFiles.Items[i].ToString() == "BankTransactions") //CHECK THE ID IN NEXUS
+                    {
+                        var bankTransactions = @"<BulkFileItem>" + "\n" +
+                           @"<BcpTimeout>PT0S</BcpTimeout>" + "\n" +
+                           @"<ID>Transactions</ID>" + "\n" +
+                           @"<LocalFile>{13}\" + local + @"\{3:MMddyyyy}_BankTransactions_" + local + @".txt</LocalFile>" + "\n" +
+                           @"<MaxRowsPerBatch i:nil=""true"" />" + "\n" +
+                           @"<Optional>false</Optional>" + "\n" +
+                           @"<RemoteFile>{0}\" + remote + @"\{3:MMddyyyy}_BankTransactions_" + remote + @".txt</RemoteFile>" + "\n" +
+                           @"<UploadFileDescription i:nil=""true"" />" + "\n" +
+                           @"<UploadFileName i:nil=""true"" />" + "\n" +
+                           @"</BulkFileItem>" + "\n";
+                        sb.Insert(s.Length - 1, bankTransactions);
+                    }
+                    else if (checkedListBulkFiles.GetItemChecked(i) && checkedListBulkFiles.Items[i].ToString() == "Client") //CHECK THE ID IN NEXUS
+                    {
+                        var client = @"<BulkFileItem>" + "\n" +
+                           @"<BcpTimeout>PT0S</BcpTimeout>" + "\n" +
+                           @"<ID>Rep_Clients</ID>" + "\n" +
+                           @"<LocalFile>{13}\" + local + @"\{3:MMddyyyy}_Client_" + local + @".txt</LocalFile>" + "\n" +
+                           @"<MaxRowsPerBatch i:nil=""true"" />" + "\n" +
+                           @"<Optional>false</Optional>" + "\n" +
+                           @"<RemoteFile>{0}\" + remote + @"\{3:MMddyyyy}_Client_" + remote + @".txt</RemoteFile>" + "\n" +
+                           @"<UploadFileDescription i:nil=""true"" />" + "\n" +
+                           @"<UploadFileName i:nil=""true"" />" + "\n" +
+                           @"</BulkFileItem>" + "\n";
+                        sb.Insert(s.Length - 1, client);
+                    }
+
+                }
+
+                txtOutput.Text = sb.ToString();
+                break;
+            }
+
         }
     }
 }
