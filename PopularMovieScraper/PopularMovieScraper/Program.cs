af8538abe0841a6e4eb17203cd27f05a16d76d72
Anthony Cirio <anthony.cirio@gmail.com>
2021-08-27 11:27:23 -0400

    Added scraping

--- PopularMovieScraper/PopularMovieScraper/Program.cs
+++ PopularMovieScraper/PopularMovieScraper/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CsvHelper;
+using HtmlAgilityPack;
+using System.IO;
+using System.Globalization;
 
 namespace PopularMovieScraper
 {
@@ -10,9 +14,26 @@ namespace PopularMovieScraper
     {
         static void Main(string[] args)
         {
-            //GitHub test
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load("https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm");
+
+            string HeaderNames = doc.DocumentNode.SelectNodes("//td[@class='titleColumn']");
+
+            var titles = new List<Row>();
+            foreach (var item in HeaderNames)
+                {
+                    titles.Add(new Row { Title = item.InnerText});
+                }
+            using (var writer = new StreamWriter("Desktop/imdb_example.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.WriteRecords(titles);
+                }
         }
+    }
+
+    public class Row 
+    {
+        public string Title {get; set;}
     }
 }
